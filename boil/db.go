d97e8262650c3cfdb3f9fd577a5861f4ef6e1d62
Aaron L <aaron@bettercoder.net>
2016-06-11 15:09:46 -0700

    Fix some documentation and ugly constant placement

--- boil/db.go
+++ boil/db.go
@@ -5,12 +5,19 @@ import (
 	"os"
 )
 
+var (
+	// currentDB is a global database handle for the package
+	currentDB Executor
+)
+
+// Executor can perform SQL queries.
 type Executor interface {
 	Exec(query string, args ...interface{}) (sql.Result, error)
 	Query(query string, args ...interface{}) (*sql.Rows, error)
 	QueryRow(query string, args ...interface{}) *sql.Row
 }
 
+// Transactor can commit and rollback, on top of being able to execute queries.
 type Transactor interface {
 	Commit() error
 	Rollback() error
@@ -18,11 +25,10 @@ type Transactor interface {
 	Executor
 }
 
+// Creator starts transactions.
 type Creator interface {
 	Begin() (*sql.Tx, error)
 }
-
-var currentDB Executor
 
 // DebugMode is a flag controlling whether generated sql statements and
 // debug information is outputted to the DebugWriter handle
@@ -33,6 +39,7 @@ var DebugMode = false
 // DebugWriter is where the debug output will be sent if DebugMode is true
 var DebugWriter = os.Stdout
 
+// Begin a transaction
 func Begin() (Transactor, error) {
 	creator, ok := currentDB.(Creator)
 	if !ok {

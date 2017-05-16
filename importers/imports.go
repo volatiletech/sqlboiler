55e00e91b4dcac20beb2f02990fb8763de18b52f
Aaron L <aaron@bettercoder.net>
2017-05-15 21:11:23 -0700

    Refactor imports into its own package
    - Doing this so that drivers and boilingcore can both use this package.
      Also enables easy configuration overrides. Export all the things.

---
+++ importers/imports.go
@@ -0,0 +1,390 @@
+// Package importers helps with dynamic imports for templating
+package importers
+
+import (
+	"bytes"
+	"fmt"
+	"sort"
+	"strings"
+)
+
+// Collection of imports for various templating purposes
+type Collection struct {
+	Standard     Set
+	TestStandard Set
+
+	Singleton     Map
+	TestSingleton Map
+
+	TestMain Map
+
+	BasedOnType Map
+}
+
+// imports defines the optional standard imports and
+// thirdParty imports (from github for example)
+type Set struct {
+	Standard   List
+	ThirdParty List
+}
+
+// Map of type -> import
+type Map map[string]Set
+
+// List of imports
+type List []string
+
+func (l List) Len() int {
+	return len(l)
+}
+
+func (l List) Swap(i, j int) {
+	l[i], l[j] = l[j], l[i]
+}
+
+func (l List) Less(i, j int) bool {
+	res := strings.Compare(strings.TrimLeft(l[i], "_ "), strings.TrimLeft(l[j], "_ "))
+	if res <= 0 {
+		return true
+	}
+
+	return false
+}
+
+// NewDefaultImports returns a default Imports struct.
+func NewDefaultImports() Collection {
+	var col Collection
+
+	col.Standard = Set{
+		Standard: List{
+			`"bytes"`,
+			`"database/sql"`,
+			`"fmt"`,
+			`"reflect"`,
+			`"strings"`,
+			`"sync"`,
+			`"time"`,
+		},
+		ThirdParty: List{
+			`"github.com/pkg/errors"`,
+			`"github.com/volatiletech/sqlboiler/boil"`,
+			`"github.com/volatiletech/sqlboiler/queries"`,
+			`"github.com/volatiletech/sqlboiler/queries/qm"`,
+			`"github.com/volatiletech/sqlboiler/strmangle"`,
+		},
+	}
+
+	col.Singleton = Map{
+		"boil_queries": {
+			ThirdParty: List{
+				`"github.com/volatiletech/sqlboiler/boil"`,
+				`"github.com/volatiletech/sqlboiler/queries"`,
+				`"github.com/volatiletech/sqlboiler/queries/qm"`,
+			},
+		},
+		"boil_types": {
+			ThirdParty: List{
+				`"github.com/pkg/errors"`,
+				`"github.com/volatiletech/sqlboiler/strmangle"`,
+			},
+		},
+	}
+
+	col.TestStandard = Set{
+		Standard: List{
+			`"bytes"`,
+			`"reflect"`,
+			`"testing"`,
+		},
+		ThirdParty: List{
+			`"github.com/volatiletech/sqlboiler/boil"`,
+			`"github.com/volatiletech/sqlboiler/randomize"`,
+			`"github.com/volatiletech/sqlboiler/strmangle"`,
+		},
+	}
+
+	col.TestSingleton = Map{
+		"boil_main_test": {
+			Standard: List{
+				`"database/sql"`,
+				`"flag"`,
+				`"fmt"`,
+				`"math/rand"`,
+				`"os"`,
+				`"path/filepath"`,
+				`"testing"`,
+				`"time"`,
+			},
+			ThirdParty: List{
+				`"github.com/kat-co/vala"`,
+				`"github.com/pkg/errors"`,
+				`"github.com/spf13/viper"`,
+				`"github.com/volatiletech/sqlboiler/boil"`,
+			},
+		},
+		"boil_queries_test": {
+			Standard: List{
+				`"bytes"`,
+				`"fmt"`,
+				`"io"`,
+				`"io/ioutil"`,
+				`"math/rand"`,
+				`"regexp"`,
+			},
+			ThirdParty: List{
+				`"github.com/volatiletech/sqlboiler/boil"`,
+			},
+		},
+		"boil_suites_test": {
+			Standard: List{
+				`"testing"`,
+			},
+		},
+	}
+
+	col.TestMain = Map{
+		"postgres": {
+			Standard: List{
+				`"bytes"`,
+				`"database/sql"`,
+				`"fmt"`,
+				`"io"`,
+				`"io/ioutil"`,
+				`"os"`,
+				`"os/exec"`,
+				`"strings"`,
+			},
+			ThirdParty: List{
+				`"github.com/pkg/errors"`,
+				`"github.com/spf13/viper"`,
+				`"github.com/volatiletech/sqlboiler/bdb/drivers"`,
+				`"github.com/volatiletech/sqlboiler/randomize"`,
+				`_ "github.com/lib/pq"`,
+			},
+		},
+		"mysql": {
+			Standard: List{
+				`"bytes"`,
+				`"database/sql"`,
+				`"fmt"`,
+				`"io"`,
+				`"io/ioutil"`,
+				`"os"`,
+				`"os/exec"`,
+				`"strings"`,
+			},
+			ThirdParty: List{
+				`"github.com/pkg/errors"`,
+				`"github.com/spf13/viper"`,
+				`"github.com/volatiletech/sqlboiler/bdb/drivers"`,
+				`"github.com/volatiletech/sqlboiler/randomize"`,
+				`_ "github.com/go-sql-driver/mysql"`,
+			},
+		},
+		"mssql": {
+			Standard: List{
+				`"bytes"`,
+				`"database/sql"`,
+				`"fmt"`,
+				`"os"`,
+				`"os/exec"`,
+				`"strings"`,
+			},
+			ThirdParty: List{
+				`"github.com/pkg/errors"`,
+				`"github.com/spf13/viper"`,
+				`"github.com/volatiletech/sqlboiler/bdb/drivers"`,
+				`"github.com/volatiletech/sqlboiler/randomize"`,
+				`_ "github.com/denisenkom/go-mssqldb"`,
+			},
+		},
+	}
+
+	// basedOnType imports are only included in the template output if the
+	// database requires one of the following special types. Check
+	// TranslateColumnType to see the type assignments.
+	col.BasedOnType = Map{
+		"null.Float32": {
+			ThirdParty: List{`"gopkg.in/nullbio/null.v6"`},
+		},
+		"null.Float64": {
+			ThirdParty: List{`"gopkg.in/nullbio/null.v6"`},
+		},
+		"null.Int": {
+			ThirdParty: List{`"gopkg.in/nullbio/null.v6"`},
+		},
+		"null.Int8": {
+			ThirdParty: List{`"gopkg.in/nullbio/null.v6"`},
+		},
+		"null.Int16": {
+			ThirdParty: List{`"gopkg.in/nullbio/null.v6"`},
+		},
+		"null.Int32": {
+			ThirdParty: List{`"gopkg.in/nullbio/null.v6"`},
+		},
+		"null.Int64": {
+			ThirdParty: List{`"gopkg.in/nullbio/null.v6"`},
+		},
+		"null.Uint": {
+			ThirdParty: List{`"gopkg.in/nullbio/null.v6"`},
+		},
+		"null.Uint8": {
+			ThirdParty: List{`"gopkg.in/nullbio/null.v6"`},
+		},
+		"null.Uint16": {
+			ThirdParty: List{`"gopkg.in/nullbio/null.v6"`},
+		},
+		"null.Uint32": {
+			ThirdParty: List{`"gopkg.in/nullbio/null.v6"`},
+		},
+		"null.Uint64": {
+			ThirdParty: List{`"gopkg.in/nullbio/null.v6"`},
+		},
+		"null.String": {
+			ThirdParty: List{`"gopkg.in/nullbio/null.v6"`},
+		},
+		"null.Bool": {
+			ThirdParty: List{`"gopkg.in/nullbio/null.v6"`},
+		},
+		"null.Time": {
+			ThirdParty: List{`"gopkg.in/nullbio/null.v6"`},
+		},
+		"null.JSON": {
+			ThirdParty: List{`"gopkg.in/nullbio/null.v6"`},
+		},
+		"null.Bytes": {
+			ThirdParty: List{`"gopkg.in/nullbio/null.v6"`},
+		},
+		"time.Time": {
+			Standard: List{`"time"`},
+		},
+		"types.JSON": {
+			ThirdParty: List{`"github.com/volatiletech/sqlboiler/types"`},
+		},
+		"types.BytesArray": {
+			ThirdParty: List{`"github.com/volatiletech/sqlboiler/types"`},
+		},
+		"types.Int64Array": {
+			ThirdParty: List{`"github.com/volatiletech/sqlboiler/types"`},
+		},
+		"types.Float64Array": {
+			ThirdParty: List{`"github.com/volatiletech/sqlboiler/types"`},
+		},
+		"types.BoolArray": {
+			ThirdParty: List{`"github.com/volatiletech/sqlboiler/types"`},
+		},
+		"types.StringArray": {
+			ThirdParty: List{`"github.com/volatiletech/sqlboiler/types"`},
+		},
+		"types.Hstore": {
+			ThirdParty: List{`"github.com/volatiletech/sqlboiler/types"`},
+		},
+	}
+
+	return col
+}
+
+func combineImports(a, b Set) Set {
+	var c Set
+
+	c.Standard = removeDuplicates(combineStringSlices(a.Standard, b.Standard))
+	c.ThirdParty = removeDuplicates(combineStringSlices(a.ThirdParty, b.ThirdParty))
+
+	sort.Sort(c.Standard)
+	sort.Sort(c.ThirdParty)
+
+	return c
+}
+
+func combineTypeImports(a Set, b map[string]Set, columnTypes []string) Set {
+	tmpImp := Set{
+		Standard:   make(List, len(a.Standard)),
+		ThirdParty: make(List, len(a.ThirdParty)),
+	}
+
+	copy(tmpImp.Standard, a.Standard)
+	copy(tmpImp.ThirdParty, a.ThirdParty)
+
+	for _, typ := range columnTypes {
+		for key, imp := range b {
+			if typ == key {
+				tmpImp.Standard = append(tmpImp.Standard, imp.Standard...)
+				tmpImp.ThirdParty = append(tmpImp.ThirdParty, imp.ThirdParty...)
+			}
+		}
+	}
+
+	tmpImp.Standard = removeDuplicates(tmpImp.Standard)
+	tmpImp.ThirdParty = removeDuplicates(tmpImp.ThirdParty)
+
+	sort.Sort(tmpImp.Standard)
+	sort.Sort(tmpImp.ThirdParty)
+
+	return tmpImp
+}
+
+func buildImportString(imps Set) []byte {
+	stdlen, thirdlen := len(imps.Standard), len(imps.ThirdParty)
+	if stdlen+thirdlen < 1 {
+		return []byte{}
+	}
+
+	if stdlen+thirdlen == 1 {
+		var imp string
+		if stdlen == 1 {
+			imp = imps.Standard[0]
+		} else {
+			imp = imps.ThirdParty[0]
+		}
+		return []byte(fmt.Sprintf("import %s", imp))
+	}
+
+	buf := &bytes.Buffer{}
+	buf.WriteString("import (")
+	for _, std := range imps.Standard {
+		fmt.Fprintf(buf, "\n\t%s", std)
+	}
+	if stdlen != 0 && thirdlen != 0 {
+		buf.WriteString("\n")
+	}
+	for _, third := range imps.ThirdParty {
+		fmt.Fprintf(buf, "\n\t%s", third)
+	}
+	buf.WriteString("\n)\n")
+
+	return buf.Bytes()
+}
+
+func combineStringSlices(a, b []string) []string {
+	c := make([]string, len(a)+len(b))
+	if len(a) > 0 {
+		copy(c, a)
+	}
+	if len(b) > 0 {
+		copy(c[len(a):], b)
+	}
+
+	return c
+}
+
+func removeDuplicates(dedup []string) []string {
+	if len(dedup) <= 1 {
+		return dedup
+	}
+
+	for i := 0; i < len(dedup)-1; i++ {
+		for j := i + 1; j < len(dedup); j++ {
+			if dedup[i] != dedup[j] {
+				continue
+			}
+
+			if j != len(dedup)-1 {
+				dedup[j] = dedup[len(dedup)-1]
+				j--
+			}
+			dedup = dedup[:len(dedup)-1]
+		}
+	}
+
+	return dedup
+}
---
+++ importers/imports_test.go
@@ -0,0 +1,215 @@
+package importers
+
+import (
+	"reflect"
+	"sort"
+	"testing"
+
+	"github.com/pkg/errors"
+)
+
+func TestImportsSort(t *testing.T) {
+	t.Parallel()
+
+	a1 := List{
+		`"fmt"`,
+		`"errors"`,
+	}
+	a2 := List{
+		`_ "github.com/lib/pq"`,
+		`_ "github.com/gorilla/n"`,
+		`"github.com/gorilla/mux"`,
+		`"github.com/gorilla/websocket"`,
+	}
+
+	a1Expected := List{`"errors"`, `"fmt"`}
+	a2Expected := List{
+		`"github.com/gorilla/mux"`,
+		`_ "github.com/gorilla/n"`,
+		`"github.com/gorilla/websocket"`,
+		`_ "github.com/lib/pq"`,
+	}
+
+	sort.Sort(a1)
+	if !reflect.DeepEqual(a1, a1Expected) {
+		t.Errorf("Expected a1 to match a1Expected, got: %v", a1)
+	}
+
+	for i, v := range a1 {
+		if v != a1Expected[i] {
+			t.Errorf("Expected a1[%d] to match a1Expected[%d]:\n%s\n%s\n", i, i, v, a1Expected[i])
+		}
+	}
+
+	sort.Sort(a2)
+	if !reflect.DeepEqual(a2, a2Expected) {
+		t.Errorf("Expected a2 to match a2expected, got: %v", a2)
+	}
+
+	for i, v := range a2 {
+		if v != a2Expected[i] {
+			t.Errorf("Expected a2[%d] to match a2Expected[%d]:\n%s\n%s\n", i, i, v, a1Expected[i])
+		}
+	}
+}
+
+func TestCombineTypeImports(t *testing.T) {
+	t.Parallel()
+
+	imports1 := Set{
+		Standard: List{
+			`"errors"`,
+			`"fmt"`,
+		},
+		ThirdParty: List{
+			`"github.com/volatiletech/sqlboiler/boil"`,
+		},
+	}
+
+	importsExpected := Set{
+		Standard: List{
+			`"errors"`,
+			`"fmt"`,
+			`"time"`,
+		},
+		ThirdParty: List{
+			`"github.com/volatiletech/sqlboiler/boil"`,
+			`"gopkg.in/nullbio/null.v6"`,
+		},
+	}
+
+	types := []string{
+		"null.Time",
+		"null.Time",
+		"time.Time",
+		"null.Float",
+	}
+
+	imps := NewDefaultImports()
+
+	res1 := combineTypeImports(imports1, imps.BasedOnType, types)
+
+	if !reflect.DeepEqual(res1, importsExpected) {
+		t.Errorf("Expected res1 to match importsExpected, got:\n\n%#v\n", res1)
+	}
+
+	imports2 := Set{
+		Standard: List{
+			`"errors"`,
+			`"fmt"`,
+			`"time"`,
+		},
+		ThirdParty: List{
+			`"github.com/volatiletech/sqlboiler/boil"`,
+			`"gopkg.in/nullbio/null.v6"`,
+		},
+	}
+
+	res2 := combineTypeImports(imports2, imps.BasedOnType, types)
+
+	if !reflect.DeepEqual(res2, importsExpected) {
+		t.Errorf("Expected res2 to match importsExpected, got:\n\n%#v\n", res1)
+	}
+}
+
+func TestCombineImports(t *testing.T) {
+	t.Parallel()
+
+	a := Set{
+		Standard:   List{"fmt"},
+		ThirdParty: List{"github.com/volatiletech/sqlboiler", "gopkg.in/nullbio/null.v6"},
+	}
+	b := Set{
+		Standard:   List{"os"},
+		ThirdParty: List{"github.com/volatiletech/sqlboiler"},
+	}
+
+	c := combineImports(a, b)
+
+	if c.Standard[0] != "fmt" && c.Standard[1] != "os" {
+		t.Errorf("Wanted: fmt, os got: %#v", c.Standard)
+	}
+	if c.ThirdParty[0] != "github.com/volatiletech/sqlboiler" && c.ThirdParty[1] != "gopkg.in/nullbio/null.v6" {
+		t.Errorf("Wanted: github.com/volatiletech/sqlboiler, gopkg.in/nullbio/null.v6 got: %#v", c.ThirdParty)
+	}
+}
+
+func TestRemoveDuplicates(t *testing.T) {
+	t.Parallel()
+
+	hasDups := func(possible []string) error {
+		for i := 0; i < len(possible)-1; i++ {
+			for j := i + 1; j < len(possible); j++ {
+				if possible[i] == possible[j] {
+					return errors.Errorf("found duplicate: %s [%d] [%d]", possible[i], i, j)
+				}
+			}
+		}
+
+		return nil
+	}
+
+	if len(removeDuplicates([]string{})) != 0 {
+		t.Error("It should have returned an empty slice")
+	}
+
+	oneItem := []string{"patrick"}
+	slice := removeDuplicates(oneItem)
+	if ln := len(slice); ln != 1 {
+		t.Error("Length was wrong:", ln)
+	} else if oneItem[0] != slice[0] {
+		t.Errorf("Slices differ: %#v %#v", oneItem, slice)
+	}
+
+	slice = removeDuplicates([]string{"hello", "patrick", "hello"})
+	if ln := len(slice); ln != 2 {
+		t.Error("Length was wrong:", ln)
+	}
+	if err := hasDups(slice); err != nil {
+		t.Error(err)
+	}
+
+	slice = removeDuplicates([]string{"five", "patrick", "hello", "hello", "patrick", "hello", "hello"})
+	if ln := len(slice); ln != 3 {
+		t.Error("Length was wrong:", ln)
+	}
+	if err := hasDups(slice); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestCombineStringSlices(t *testing.T) {
+	t.Parallel()
+
+	var a, b []string
+	slice := combineStringSlices(a, b)
+	if ln := len(slice); ln != 0 {
+		t.Error("Len was wrong:", ln)
+	}
+
+	a = []string{"1", "2"}
+	slice = combineStringSlices(a, b)
+	if ln := len(slice); ln != 2 {
+		t.Error("Len was wrong:", ln)
+	} else if slice[0] != a[0] || slice[1] != a[1] {
+		t.Errorf("Slice mismatch: %#v %#v", a, slice)
+	}
+
+	b = a
+	a = nil
+	slice = combineStringSlices(a, b)
+	if ln := len(slice); ln != 2 {
+		t.Error("Len was wrong:", ln)
+	} else if slice[0] != b[0] || slice[1] != b[1] {
+		t.Errorf("Slice mismatch: %#v %#v", b, slice)
+	}
+
+	a = b
+	b = []string{"3", "4"}
+	slice = combineStringSlices(a, b)
+	if ln := len(slice); ln != 4 {
+		t.Error("Len was wrong:", ln)
+	} else if slice[0] != a[0] || slice[1] != a[1] || slice[2] != b[0] || slice[3] != b[1] {
+		t.Errorf("Slice mismatch: %#v + %#v != #%v", a, b, slice)
+	}
+}

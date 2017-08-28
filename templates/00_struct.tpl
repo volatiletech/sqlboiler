volatiletech/sqlboiler

commit 6b7351d789f023a6807e006b419392d19d3cc5be

#191. Configurable struct tags casing by cmdline argument struct-tag-casing

templates/00_struct.tpl
@@ -5,10 +5,15 @@
 {{- $tableNameSingular := .Table.Name | singular -}}
 {{- $modelName := $tableNameSingular | titleCase -}}
 {{- $modelNameCamel := $tableNameSingular | camelCase -}}
+
 // {{$modelName}} is an object representing the database table.
 type {{$modelName}} struct {
-	{{range $column := .Table.Columns -}}
-	{{titleCase $column.Name}} {{$column.Type}} `{{generateTags $dot.Tags $column.Name}}boil:"{{$column.Name}}" json:"{{$column.Name}}{{if $column.Nullable}},omitempty{{end}}" toml:"{{$column.Name}}" yaml:"{{$column.Name}}{{if $column.Nullable}},omitempty{{end}}"`
+	{{range $column := .Table.Columns }}
+    {{- if eq $dot.StructTagCasing "camel"}}
+    {{titleCase $column.Name}} {{$column.Type}} `{{generateTags $dot.Tags $column.Name}}boil:"{{$column.Name}}" json:"{{$column.Name | camelCase}}{{if $column.Nullable}},omitempty{{end}}" toml:"{{$column.Name | camelCase}}" yaml:"{{$column.Name | camelCase}}{{if $column.Nullable}},omitempty{{end}}"`
+    {{- else -}}
+    {{titleCase $column.Name}} {{$column.Type}} `{{generateTags $dot.Tags $column.Name}}boil:"{{$column.Name}}" json:"{{$column.Name}}{{if $column.Nullable}},omitempty{{end}}" toml:"{{$column.Name}}" yaml:"{{$column.Name}}{{if $column.Nullable}},omitempty{{end}}"`
+    {{end -}}
 	{{end -}}
 	{{- if .Table.IsJoinTable -}}
 	{{- else}}

boilingcore/boilingcore.go
@@ -107,6 +107,7 @@ func (s *State) Run(includeTests bool) error {
 		PkgName:          s.Config.PkgName,
 		NoHooks:          s.Config.NoHooks,
 		NoAutoTimestamps: s.Config.NoAutoTimestamps,
+		StructTagCasing:  s.Config.StructTagCasing,
 		Dialect:          s.Dialect,
 		LQ:               strmangle.QuoteCharacter(s.Dialect.LQ),
 		RQ:               strmangle.QuoteCharacter(s.Dialect.RQ),
@@ -142,6 +143,7 @@ func (s *State) Run(includeTests bool) error {
 			PkgName:          s.Config.PkgName,
 			NoHooks:          s.Config.NoHooks,
 			NoAutoTimestamps: s.Config.NoAutoTimestamps,
+			StructTagCasing:  s.Config.StructTagCasing,
 			Tags:             s.Config.Tags,
 			Dialect:          s.Dialect,
 			LQ:               strmangle.QuoteCharacter(s.Dialect.LQ),

boilingcore/config.go
@@ -16,6 +16,7 @@ type Config struct {
 	NoHooks          bool
 	NoAutoTimestamps bool
 	Wipe             bool
+	StructTagCasing  string
 
 	Postgres PostgresConfig
 	MySQL    MySQLConfig

boilingcore/templates.go
@@ -34,6 +34,9 @@ type templateData struct {
 	// Tags control which
 	Tags []string
 
+	// Generate struct tags as camelCase or snake_case
+	StructTagCasing string
+
 	// StringFuncs are usable in templates with stringMap
 	StringFuncs map[string]func(string) string
 

main.go
@@ -87,6 +87,7 @@ func main() {
 	rootCmd.PersistentFlags().BoolP("version", "", false, "Print the version")
 	rootCmd.PersistentFlags().BoolP("tinyint-as-bool", "", false, "Map MySQL tinyint(1) in Go to bool instead of int8")
 	rootCmd.PersistentFlags().BoolP("wipe", "", false, "Delete the output folder (rm -rf) before generation to ensure sanity")
+	rootCmd.PersistentFlags().StringP("struct-tag-casing", "", "snake", "Decides the casing for go structure tag names. camel or snake (default snake)")
 
 	// hide flags not recommended for use
 	rootCmd.PersistentFlags().MarkHidden("replace")
@@ -141,6 +142,7 @@ func preRun(cmd *cobra.Command, args []string) error {
 		NoHooks:          viper.GetBool("no-hooks"),
 		NoAutoTimestamps: viper.GetBool("no-auto-timestamps"),
 		Wipe:             viper.GetBool("wipe"),
+		StructTagCasing:  strings.ToLower(viper.GetString("struct-tag-casing")), // camel | snake
 	}
 
 	// BUG: https://github.com/spf13/viper/issues/200

commit abd56d91826a5f7385a4b7487856793ae4913af1

Fix indentation that was broken by PR #194

templates/00_struct.tpl
@@ -9,11 +9,11 @@
 // {{$modelName}} is an object representing the database table.
 type {{$modelName}} struct {
 	{{range $column := .Table.Columns }}
-    {{- if eq $dot.StructTagCasing "camel"}}
-    {{titleCase $column.Name}} {{$column.Type}} `{{generateTags $dot.Tags $column.Name}}boil:"{{$column.Name}}" json:"{{$column.Name | camelCase}}{{if $column.Nullable}},omitempty{{end}}" toml:"{{$column.Name | camelCase}}" yaml:"{{$column.Name | camelCase}}{{if $column.Nullable}},omitempty{{end}}"`
-    {{- else -}}
-    {{titleCase $column.Name}} {{$column.Type}} `{{generateTags $dot.Tags $column.Name}}boil:"{{$column.Name}}" json:"{{$column.Name}}{{if $column.Nullable}},omitempty{{end}}" toml:"{{$column.Name}}" yaml:"{{$column.Name}}{{if $column.Nullable}},omitempty{{end}}"`
-    {{end -}}
+	{{- if eq $dot.StructTagCasing "camel"}}
+	{{titleCase $column.Name}} {{$column.Type}} `{{generateTags $dot.Tags $column.Name}}boil:"{{$column.Name}}" json:"{{$column.Name | camelCase}}{{if $column.Nullable}},omitempty{{end}}" toml:"{{$column.Name | camelCase}}" yaml:"{{$column.Name | camelCase}}{{if $column.Nullable}},omitempty{{end}}"`
+	{{- else -}}
+	{{titleCase $column.Name}} {{$column.Type}} `{{generateTags $dot.Tags $column.Name}}boil:"{{$column.Name}}" json:"{{$column.Name}}{{if $column.Nullable}},omitempty{{end}}" toml:"{{$column.Name}}" yaml:"{{$column.Name}}{{if $column.Nullable}},omitempty{{end}}"`
+	{{end -}}
 	{{end -}}
 	{{- if .Table.IsJoinTable -}}
 	{{- else}}
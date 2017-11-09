be6d27f6386b60736b75bd8feefee9154436faa5
knqyf263 <knqyf263@gmail.com>
2017-11-09 11:15:42 +0900

    Fix HAVING clause

--- queries/_fixtures/08.sql
+++ queries/_fixtures/08.sql
@@ -1,1 +1,1 @@
-SELECT * FROM "a" WHERE (a=$1 or b=$2) AND (c=$3) GROUP BY id, name HAVING id <> $4, length(name, $5) > $6;
+SELECT * FROM "a" WHERE (a=$1 or b=$2) AND (c=$3) GROUP BY id, name HAVING id <> $4 AND length(name, $5) > $6;

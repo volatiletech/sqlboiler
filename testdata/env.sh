db6356ffa7cf588514a7feb986dce6b54907e42e
Gorka Lerchundi Osa <glertxundi@gmail.com>
2018-03-19 09:23:07 +0100

    build: make use of envs instead of args
    Fixes #257
    
    Signed-off-by: Gorka Lerchundi Osa <glertxundi@gmail.com>

---
+++ testdata/env.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+SQLBOILER_TEST_USER="sqlboiler_root_user"
+SQLBOILER_TEST_DB="sqlboiler_model_test"
+SQLBOILER_TEST_PASS="Sqlboiler@1234"
+
+SQLBOILER_TEST_DRIVER_DB="sqlboiler_driver_test"
+SQLBOILER_TEST_DRIVER_USER="sqlboiler_driver_user"
+SQLBOILER_TEST_DRIVER_PASS="Sqlboiler@1234"

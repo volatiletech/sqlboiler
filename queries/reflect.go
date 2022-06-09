e02c8a67cc944489cc6807e151da65a0949b3e50
Jack Edwards <jedwards@greenlighthealth.com>
2022-06-09 17:11:38 -0400

    clairfy what Bind does when boil tags are duplicated

--- queries/reflect.go
+++ queries/reflect.go
@@ -77,6 +77,8 @@ func (q *Query) BindG(ctx context.Context, obj interface{}) error {
 //   - If the ",bind" option is specified on a struct field and that field
 //     is a struct itself, it will be recursed into to look for fields for
 //     binding.
+//   - If one or more boil struct tags are duplicated the behaviour of Bind
+//     is undefined.
 //
 // Example usage:
 //
